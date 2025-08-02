ae2831695763ea3c3cb06a488c1c1190588fa1d0
Vinicius Camilotti <vinicamilotti@gmail.com>
2025-08-02 15:11:19 -0300

    UI corrigida nas fases

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    bool started = false;
+    public GameObject libas;
+
+    public void StartGame()
     {
-        
+        started = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        if (!started)
+        {
+            return;
+        }
+        Vector2 movement = new Vector2(1, 0);
+        libas.transform.Translate(movement * 20f * Time.deltaTime);
+
+        if (libas.transform.position.x < 24f)
+        {
+            return;
+        }
+
+        SceneManager.LoadScene("Level1");
+
     }
 }
