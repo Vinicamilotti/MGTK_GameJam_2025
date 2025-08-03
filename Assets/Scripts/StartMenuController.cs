4d981f095d5919f5f0f1c86f7f3ecd025401d320
Vinicius Camilotti <vinicamilotti@gmail.com>
2025-08-03 11:22:44 -0300

    playing in loop + add doppler

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -4,8 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour
 {
+    bool audioIsPlaying = false;
     bool started = false;
     public GameObject libas;
+    AudioSource libasAudioSource;
+    
+    void Start() {
+        libasAudioSource = libas.GetComponent<AudioSource>();
+    }
 
     public void StartGame()
     {
@@ -19,15 +25,23 @@ public class StartMenuController : MonoBehaviour
         {
             return;
         }
+        
+        if(!audioIsPlaying) {
+            libasAudioSource.Play();
+            audioIsPlaying = true;
+        }
+       
         Vector2 movement = new Vector2(1, 0);
         libas.transform.Translate(movement * 20f * Time.deltaTime);
 
-        if (libas.transform.position.x < 24f)
+        if (libas.transform.position.x < 26f)
         {
             return;
         }
 
+   
         SceneManager.LoadScene("Level1");
+        libasAudioSource.Stop();
 
     }
 }
