4ac8b965250ba244f9f260bacf9c371ca1b683ca
Vinicius Camilotti <vinicamilotti@gmail.com>
2025-08-03 12:55:26 -0300

    fixed sound overlaping bug

--- Assets/Scripts/EndSceneController.cs
+++ Assets/Scripts/EndSceneController.cs
@@ -7,6 +7,8 @@ public class EndSceneController : MonoBehaviour
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Restart() {
+        var sound = GameObject.Find("SoundManager");
+        Destroy(sound);
         SceneManager.LoadScene("Tutorial");
     }
     // Update is called once per frame
